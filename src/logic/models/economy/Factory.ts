c19322e59af0275678be0588c9f3654c2053dcdd
Papiricoh <slendeman0200@gmail.com>
2024-06-08 02:29:38 +0200

    added first steps checkProduction

--- src/logic/models/economy/Factory.ts
+++ src/logic/models/economy/Factory.ts
@@ -7,20 +7,33 @@ export default class Factory {
     input: number[];
     output: Good[];
     size: number;
+    workers: number;
   
-    constructor(name: string, neededGoods: NeededGood[], input: number[], output: Good[], size: number) {
+    constructor(name: string, neededGoods: NeededGood[], input: number[], output: Good[], size: number, workers: number) {
         this.name = name;
         this.neededInput = neededGoods;
         this.input = input;
         this.output = output;
         this.size = size;
+        this.workers = workers;
+    }
+
+    checkProduction() {
+        let neededImput = 0;
+        for (let index = 0; index < this.neededInput.length; index++) {
+            neededImput += this.neededInput[index].quantity;
+        }
+        let avariableInput: number = 0;
+        for (let index = 0; index < this.neededInput.length; index++) {
+            avariableInput += this.input[index];
+        }
+        let outputFactor = neededImput - avariableInput;
 
     }
 
 
-
     getOutput(): Number {
-        
+
         return 0;
     }
   
