619ea44cc1c881a8d4d19cb80b8fbd507ada59e3
Papiricoh <slendeman0200@gmail.com>
2024-06-17 16:09:42 +0200

    fixed incremental effect

--- src/components/visual/IncrementalNumber.vue
+++ src/components/visual/IncrementalNumber.vue
@@ -31,8 +31,9 @@ export default defineComponent({
             this.starting_num = 0;
             this.intervalId = setInterval(() => {
                 if (this.starting_num < target) {
-                    this.starting_num += 100;
+                    this.starting_num += 123;
                 } else {
+                    this.starting_num = target;
                     clearInterval(this.intervalId);
                 }
             }, 1); // Cambia 50 por el valor en ms que desees para la velocidad de incremento.
